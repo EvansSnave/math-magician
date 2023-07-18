9911960571dca13b1efb748bfddd437ae7e67c5e
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-17 20:38:35 -0500

    make first components

---
+++ src/components/Calculator.js
@@ -0,0 +1,31 @@
+import Output from './Output';
+
+function Calculator() {
+  return (
+  <div class="calculator">
+    <Output />
+    <div class="calculator__input">
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+      <button class="input__clear">AC</button>
+    </div>
+  </div>
+  );
+}
+
+export default Calculator;
---
+++ src/components/Output.js
@@ -0,0 +1,5 @@
+function Output() {
+  return <div class="calculator__output">0</div>
+}
+
+export default Output;
